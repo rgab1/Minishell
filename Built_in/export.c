e2b48e1bbcf16c19096fa34bae35d5c134021f1c
rgab1 <rivaultgabin@gmail.com>
2026-06-18 05:55:37 +0200

    feat: wip export is getting finalized

--- Built_in/export.c
+++ Built_in/export.c
@@ -6,21 +6,41 @@
 /*   By: grivault <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/02 21:09:27 by grivault          #+#    #+#             */
-/*   Updated: 2026/05/11 03:05:20 by grivault         ###   ########.fr       */
+/*   Updated: 2026/06/18 05:40:24 by grivault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
+static size_t	env_count(t_env *env)
+{
+	size_t	i;
+
+	i = 0;
+	while (env)
+	{
+		env = env->next;
+		i++;
+	}
+	return (i);
+}
+
+
 //a coder: print_env_alphabetical
 
 int	export(t_shell *shell)
 {
-	if (!shell->cmd->cmd)
-		return (exit_error("Error a definir", 3), 1);
-	if (shell->cmd->cmd[1])
-		new_env_node(&shell->env, extract_key_value(shell->cmd->cmd[1]));
-//	else
-//		print_env_alphabetical(shell->env);
+	size_t	i;
+	char	**pair;
+
+	i = 1;
+	if (!shell->cmd->cmd[1])
+		return (print_env_alphabetical(shell->env), 0);
+	while (shell->cmd->cmd[i])
+	{
+		pair = extract_key_value(shell->cmd->cmd[i++]);
+		set_value(pair[0], pair[1], &shell->env);
+		free_strings(pair);
+	}
 	return (0);
 }
