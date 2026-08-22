db5d0c49261590267441a40fdda9faf4b147dc48
grivault <grivault@f1br3s3.paris.42.school>
2026-08-22 17:28:36 +0200

    fix: fix double close on cmd fd

--- Execution/run_pipeline.c
+++ Execution/run_pipeline.c
@@ -6,7 +6,7 @@
 /*   By: hassmou <hassmou@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/18 17:49:06 by grivault          #+#    #+#             */
-/*   Updated: 2026/08/06 16:27:54 by hassmou          ###   ########.fr       */
+/*   Updated: 2026/08/22 17:28:02 by grivault         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,19 @@ static void	pipe_assignement(int *fd, t_cmd *current)
 	current->next->in_fd = fd[0];
 }
 
+static void	close_fds(t_shell *shell)
+{
+	if (shell->cmd->in_fd > 2)
+	{
+		close(shell->cmd->in_fd);
+		shell->cmd->in_fd = -2;
+	}
+	if (shell->cmd->out_fd > 2)
+	{
+		close(shell->cmd->out_fd);
+		shell->cmd->out_fd = -2;
+	}
+}
 void	run_pipeline(t_shell *shell, int *pid)
 {
 	int		fd[2];
@@ -38,10 +51,7 @@ void	run_pipeline(t_shell *shell, int *pid)
 			is_builtin(shell, pid);
 			run_command(shell->cmd, get_envp(shell), shell);
 		}
-		if (shell->cmd->in_fd > 2)
-			close(shell->cmd->in_fd);
-		if (shell->cmd->out_fd > 2)
-			close(shell->cmd->out_fd);
+		close_fds(shell);
 		temp = shell->cmd->next;
 		free_cmd(shell->cmd);
 		shell->cmd = temp;
